21eeff3253c6be5e242aa3a1f80d7c04ca2c1d1d
sling254 <mwanikidenis7@gmail.com>
2021-12-10 22:43:36 +0300

    add signals to create user profile on save

--- projects/models.py
+++ projects/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 
@@ -10,3 +12,17 @@ class UserProfile(models.Model):
     birth_date = models.DateField(blank=True,null=True)
     location = models.CharField(max_length=50,blank=True,null=True)
     picture = models.ImageField(upload_to='static/profile_pics',default='static/usericon.png',blank=True)
+
+    def __str__(self):
+        return f"{self.user.name}'s Profile"
+
+    
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
