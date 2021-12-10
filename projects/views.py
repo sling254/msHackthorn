32cc1a606482c1ced6a40b3aad87887cb5da3352
sling254 <mwanikidenis7@gmail.com>
2021-12-10 22:56:30 +0300

    add user edit profile view

--- projects/views.py
+++ projects/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import View
 from .models import UserProfile
-
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, UpdateView, CreateView, DeleteView
 
 # Create your views here.
 
@@ -21,3 +23,20 @@ class ProfileView(View):
         }
 
         return render(request, 'profile.html', context)
+
+
+class ProfileEditView(LoginRequiredMixin, UserPassesTestMixin,UpdateView):
+    model = UserProfile
+    fields = ['name', 'bio', 'birth_date', 'location', 'picture']
+    template_name = 'profile_edit.html'
+
+    def get_success_url(self):
+        pk = self.kwargs['pk']
+        return reverse_lazy('profile', kwargs={'pk': pk})
+        
+
+    def test_func(self):
+        profile = self.get_object()
+        if self.request.user == profile.user:
+            return True
+        return False
