083c4247cd33710939ba820f576bd44501054a50
sling254 <mwanikidenis7@gmail.com>
2021-12-11 22:58:47 +0300

    add  post html

--- projects/models.py
+++ projects/models.py
@@ -35,8 +35,8 @@ class Project(models.Model):
     description = models.TextField(max_length=500,blank=True,null=True)
     image = CloudinaryField('images',blank=True)
     live_link = models.CharField(max_length=50,blank=True,null=True)
-    github_like = models.CharField(max_length=50,blank=True,null=True)
-    created_on = models.DateTimeField(auto_now_add=True)
+    github_link = models.CharField(max_length=50,blank=True,null=True)
+    created_on = models.DateTimeField(auto_now_add=True,null=True,blank=True)
 
     def __str__(self):
         return f"{self.user.username}'s Project"
