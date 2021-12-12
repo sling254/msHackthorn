f029e19e43e472b4b787699374ac0162a1536c56
sling254 <mwanikidenis7@gmail.com>
2021-12-12 14:19:27 +0300

    add userprofileserializer

--- awardapi/serializers.py
+++ awardapi/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
-from projects.models import Project
+from projects.models import Project,UserProfile
 
 
 class ProjectSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Project
         fields = ('id', 'title', 'description', 'live_link', 'github_link', 'created_on')
+
+
+class UserProfileSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = UserProfile
+        fields = ('id', 'username', 'bio', 'birth_date','picture', 'location','email','picture')
