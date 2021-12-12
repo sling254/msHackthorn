0b6b29545d6c13dbb3e5ffba03af907b3511c37f
sling254 <mwanikidenis7@gmail.com>
2021-12-12 14:21:05 +0300

    add userprofile views

--- awardapi/views.py
+++ awardapi/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from projects.models import Project
-from .serializers import ProjectSerializer
+from projects.models import Project, UserProfile
+from .serializers import ProjectSerializer, UserProfileSerializer
 # Create your views here.
 
 class ProjectViewSet(viewsets.ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = UserProfile.objects.all()
+    serializer_class = UserProfileSerializer
